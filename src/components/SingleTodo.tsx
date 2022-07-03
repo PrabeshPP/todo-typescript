28e9e5f414bf512d246339ef3e778a33b4ed7630
Prabesh <prabesh.bista369@gmail.com>
2022-07-03 20:53:40 +0545

    strike through added

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FC, useRef, useState } from "react";
 import "./todo.css";
 import { BsCheckLg } from "react-icons/bs";
 import { AiFillDelete, AiFillEdit } from "react-icons/ai";
@@ -10,16 +10,36 @@ interface Props {
   setTodos:React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const SingleTodo = ({ todo }: Props) => {
+const SingleTodo = ({ todo,setTodos,todos }: Props) => {
+  const [editable,setIsEditable]=useState<boolean>(false);
+  
+  const handlerDone=(id:number)=>{
+     setTodos(todos.map((item)=>item.id===id?{...item,isDone:!item.isDone}:item))
+  }
+
+  const onClickHandler=()=>{
+      setIsEditable(!editable);
+     
+  }
+
+
   return (
     <form className="box">
       <div className="textBox">
-        <h1 contentEditable="true">{todo.todo}</h1>
+        {
+          todo.isDone?
+          <s   contentEditable={editable}>{todo.todo}</s>
+          :<span   contentEditable={editable}>{todo.todo}</span>
+        }
       </div>
       <div className="iconBox">
-        <AiFillEdit />
+        <AiFillEdit onClick={onClickHandler} />
         <AiFillDelete />
-        <BsCheckLg />
+        <BsCheckLg onClick={
+          ()=>{
+            handlerDone(todo.id); 
+          }
+        }/>
       </div>
     </form>
   );
